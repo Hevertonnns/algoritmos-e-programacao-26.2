c60ad1854212b4158b96386b21f8ce4c072fa096
Hevertonnns <hvntom@gmail.com>
2026-03-23 17:18:17 -0300

    feat: exercicio 6 desafios

---
+++ aula_2/exercicio6.py
@@ -0,0 +1,9 @@
+#coversao de medida (metros)
+
+metros= float(input("Quantos metros deseja converter? "))
+centimestros = metros *100
+milimetros = metros *1000
+kilometros = metros / 1000
+milha = metros /  1609
+
+print(f"O valor de metros para centímetros é {centimestros}cm + para milímetros: {milimetros}mm + para kilometros: {kilometros}km + para milha: {milha}!1")
